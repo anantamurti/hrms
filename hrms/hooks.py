87da46e705d0dbd040ffe969d834a946fcfc8698
Ananta Murti <ananta.murti@gmail.com>
2025-03-30 17:41:06 +0700

    fix(config): Set to own repo

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -4,8 +4,8 @@ app_publisher = "Frappe Technologies Pvt. Ltd."
 app_description = "Modern HR and Payroll Software"
 app_email = "contact@frappe.io"
 app_license = "GNU General Public License (v3)"
-required_apps = ["frappe/erpnext"]
-source_link = "http://github.com/frappe/hrms"
+required_apps = ["anantamurti/erpnext"]
+source_link = "http://github.com/anantamurti/hrms"
 
 
 # Includes in <head>
